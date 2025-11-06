d1f35232b3a688b7df288729480d39be92c55e9a
OlyaMraka <mrakaolga@gmail.com>
2025-11-06 23:59:57 +0200

    changed migrations

---
+++ KeyKeepers.DAL/Migrations/20251106215636_DatabaseFix.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KeyKeepers.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CommunityUsers_UserName",
+                table: "CommunityUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "CommunityUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "CommunityUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "CommunityUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "CommunityUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommunityUsers_UserName",
+                table: "CommunityUsers",
+                column: "UserName",
+                unique: true);
+        }
+    }
+}
